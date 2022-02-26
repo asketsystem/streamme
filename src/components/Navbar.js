33012d5681a22d471596dc83ff1052b72a056a46
asketsystem <asketsystem1@gmail.com>
2022-02-26 16:05:10 +0000

    adding the imports and the styled component in the file Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,77 @@
 import React from "react";
+import { ArrowDropDown, Notifications, Search } from "@material-ui/icons";
+import styled, { css } from "styled-components";
+
+const NavContainer = styled.div`
+  width: 100%;
+  color: white;
+  font-size: 14px;
+  position: fixed;
+  top: 0;
+  z-index: 999;
+  background: linear-gradient(to top, transparent 0%, rgb(0, 0, 0, 0.3) 50%);
+`;
+const MainContainer = styled.div`
+  padding: 0px 50px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  height: 70px;
+`;
+const LeftContainer = styled.div`
+  display: flex;
+  align-items: center;
+  img {
+    height: 25px;
+    margin-right: 40px;
+  }
+  span {
+    margin-right: 20px;
+    cursor: pointer;
+  }
+`;
+const RightContainer = styled.div`
+  display: flex;
+  align-items: center;
+  img {
+    width: 30px;
+    height: 30px;
+    border-radius: 5px;
+    object-fit: cover;
+    cursor: pointer;
+  }
+  span {
+    padding: 10px;
+    cursor: pointer;
+  }
+`;
+const ProfileContainer = styled.div`
+  .options {
+    display: none;
+    background-color: #0b0b0b;
+    border-radius: 5px;
+  }
+  &:hover {
+    .options {
+      display: flex;
+      flex-direction: column;
+      position: absolute;
+    }
+  }
+`;
+const sharedStyle = css`
+  margin: 0px 15px;
+  cursor: pointer;
+`;
+const MySearch = styled(Search)`
+  ${sharedStyle}
+`;
+const MyNotifications = styled(Notifications)`
+  ${sharedStyle}
+`;
+const MyArrowDropDown = styled(ArrowDropDown)`
+  ${sharedStyle}
+`;
 
 const Navbar = () => {
   return (
@@ -15,7 +88,7 @@ const Navbar = () => {
         <RightContainer>
           <MySearch />
           <span>KID</span>
-          <MyNotification />
+          <MyNotifications />
           <img
             src="https://randomuser.me/api/portraits/women/1.jpg"
             alt="user-image"
