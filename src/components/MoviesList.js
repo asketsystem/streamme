714b36c3c8ddc5fb3c4a1bfd73fcc67deb00534f
asketsystem <asketsystem1@gmail.com>
2022-02-26 22:44:46 +0000

    Adding container

--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -6,13 +6,68 @@ import {
 import MovieItem from "./MovieItem";
 import styled from "styled-components";
 
+const ListContainer = styled.div`
+  width: 100%;
+  margin-top: 10px;
+`;
+// const Title = styled.span`
+//   color: white;
+//   font-size: 20px;
+//   font-weight: 500;
+//   margin-left: 50px;
+// `;
+const Wrapper = styled.div`
+  position: relative;
+  .sliderArrow {
+    width: 50px;
+    height: 100%;
+    background-color: rgb(22, 22, 22, 0.5);
+    color: white;
+    position: absolute;
+    z-index: 99;
+    top: 0;
+    bottom: 0;
+    margin: auto;
+    cursor: pointer;
+    &.left {
+      left: 0;
+    }
+    &.right {
+      right: 0;
+    }
+  }
+`;
+const MovieContainer = styled.div`
+  margin-left: 50px;
+  display: flex;
+  margin-top: 10px;
+  width: max-content;
+  transform: translateX(0px);
+  transition: all 1s ease;
+`;
+
 const MoviesList = () => {
+  const listRef = useRef();
+
+  const handleClick = (direction) => {
+    let distance = listRef.current.getBoundingClientRect().x - 50;
+    if (direction == "left") {
+      listRef.current.style.transform = `translateX(${230 + distance}px)`;
+    }
+    if (direction === "right") {
+      listRef.current.style.transform = `translateX(${-230 + distance}px)`;
+    }
+  };
+
   return (
     <ListContainer>
       <Title>Continue to watch</Title>
       <Wrapper>
-        <ArrowBackIosOutlined className="sliderArrow left" />
-        <MovieContainer>
+        <ArrowBackIosOutlined
+          className="sliderArrow left"
+          onClick={() => handleClick("right")}
+        />
+        <MovieContainer ref={listRef}>
           <MovieItem index={0} />
           <MovieItem index={1} />
           <MovieItem index={2} />
@@ -24,7 +79,10 @@ const MoviesList = () => {
           <MovieItem index={8} />
           <MovieItem index={9} />
         </MovieContainer>
-        <ArrowForwardIosOutlined className="sliderArrow right" />
+        <ArrowForwardIosOutlined
+          className="sliderArrow right"
+          onClick={() => handleClick("left")}
+        />
       </Wrapper>
     </ListContainer>
   );
