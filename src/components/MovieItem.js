c2096d1899b09979a9fc5984475c9343683cc848
asketsystem <asketsystem1@gmail.com>
2022-02-27 21:22:40 +0000

    Adding video component

--- src/components/MovieItem.js
+++ src/components/MovieItem.js
@@ -1,3 +1,4 @@
+import { PlayArrow, ThumbDownAltOutlined } from "@material-ui/icons";
 import styled from "styled-components";
 
 const ItemContainer = styled.div`
@@ -17,12 +18,42 @@ const MainImage = styled.img`
 `;
 
 const MovieItem = ({ index }) => {
+  const [isHovered, setIsHovered] = useState(false);
+  const trailer =
+    "https://test-videos.co.uk/vids/bigbuckbunny/mp4/h264/1080/Big_Buck_Bunny_1080_10s_1MB.pm4";
   return (
-    <ItemContainer>
+    <ItemContainer
+      style={{ left: isHovered && index * 225 - 50 + index * 2.5 }}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
       <MainImage
         src="https://cdn.cloudfare.steamstatic.com/steam/apps/1265780/capsule_616x353.jpg?t=1618237900"
         alt="trailer"
       />
+      {isHovered && (
+        <>
+          <Video src={trailer} autoPlay={true} loop />
+          <ItemInfo>
+            <IconsContainer>
+              <PlayArrow className="icon" />
+              <Add className="icon" />
+              <ThumbUpAltOutlined className="icon" />
+              <ThumbDownAltOutlined className="icon" />
+            </IconsContainer>
+            <InfoTop>
+              <span>2 hour 14 mins</span>
+              <span className="limit">13+</span>
+              <span>2022</span>
+            </InfoTop>
+            <Description>
+              The Lord of the Rings: Gollum is an upcoming action-adventure game
+              developed by Daedalic Entertainment.
+            </Description>
+            <Genre>Adventure</Genre>
+          </ItemInfo>
+        </>
+      )}
     </ItemContainer>
   );
 };
